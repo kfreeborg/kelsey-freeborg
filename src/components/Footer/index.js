95a21c37f7a9f18889476f77ca3653b80c29c1a9
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-02-27 10:57:31 -0700

    added icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 function Footer(props) {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer class="footer">
+      <h4>
+        <a
+          className="contact-icons"
+          href="https://github.com/kfreeborg"
+          target="_blank"
+        >
+          <FaGithub />
+        </a>
+        <a
+          className="contact-icons"
+          href="https://www.instagram.com/aboxofkelsey/"
+          target="_blank"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          className="contact-icons"
+          href="https://www.linkedin.com/in/kelsey-freeborg-7257a589/"
+          target="_blank"
+        >
+          <FaLinkedin />
+        </a>
+      </h4>
+    </footer>
   );
 }
 
